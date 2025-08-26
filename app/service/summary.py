8fc01bbf2ad9af1f86e0c2c35baa9127bbe1c4a7
HJH <nahjh2@gmail.com>
2025-08-26 15:02:29 +0900

    [#13] remove date filter
    일주일 범위에서 찾기에는 현재 활성화 되어있지 않아서 글들이 너무 적음

--- app/service/summary.py
+++ app/service/summary.py
@@ -89,7 +89,7 @@ def getPopularSummaryListService(
     from_ = datetime.now(timezone.utc) - timedelta(days=7)
     summaries = (
         db.query(Summary)
-        .filter(Summary.created >= from_)
+        # .filter(Summary.created >= from_)  # TODO: 현재 글이 너무 적어서 전체 범위에서 찾도록 함
         .join(Summary.book)
         .join(Summary.user)
         .options(contains_eager(Summary.book))
